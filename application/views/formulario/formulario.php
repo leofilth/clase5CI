e6e81f3631b5ee6dff9d1f81b5fd570e94ebb9f1
leofilth <leofilth@hotmail.com>
2016-03-22 12:47:35 -0300

    agregado un poco de css

--- application/views/formulario/formulario.php
+++ application/views/formulario/formulario.php
@@ -17,72 +17,96 @@
     );
     echo form_hidden($datos);
     ?>
-    <hr/>
-    <label class="control-label">Campo input text: </label><?php
-    $js='onClick-"algunaFuncion()"';
-    $datos=array
-    (
-        'name'=>'username',
-        'id'=>'username',
-        'value'=>'leonardo',
-        'maxlength'=>'100',
-        'class'=>'form-control'
-    );
-    echo form_input($datos,'',$js);
-    ?>
-    <label class="control-label">Campo input password text: </label><?php
-    //$js='onClick-"algunaFuncion()"';
-    $datos=array
-    (
-        'name'=>'pass',
-        'type'=>'password',
-        'id'=>'pass',
-        'maxlength'=>'100',
-        'class'=>'form-control'
-    );
-    echo form_input($datos);
-    ?>
-    <label class="control-label">Campo File </label><?php
-    //$js='onClick-"algunaFuncion()"';
-    $datos=array
-    (
-        'name'=>'archivo',
-        'id'=>'file',
-        'type'=>'file',
-        //'class'=>'form-control'
-    );
-    echo form_input($datos);
-    ?>
-    <label class="control-label">Text Area </label><?php
-    //$js='onClick-"algunaFuncion()"';
-    $datos=array
-    (
-        'name'=>'textarea',
-        'id'=>'textarea',
-        'type'=>'textarea',
-        'class'=>'form-control'
-    );
-    echo form_textarea($datos);
-    ?>
-    <label class="form-group">Campo de tipo Select:</label>
-    <select class="form-control" name="paises">
-        <option value="chile">Chile</option>
-        <option value="venezuela">Venezuela</option>
-        <option value="ecuador">Ecuador</option>
-        <option value="peru">Perú</option>
-    </select>
-        <hr/>
-    <?php
-    $datos=array
-    (
-        'name'=>'boton',
-        'id'=>'boton',
-        'type'=>'submit',
-        'class'=>'form-control btn-xs',
-        'value'=>'Enviar Mensaje'
-    );
-    echo form_submit($datos);
-    ?>
+    <div class="row">
+        <div class="col-md-5">
+            <label class="control-label">Campo input text: </label>
+            <?php
+                $js='onClick-"algunaFuncion()"';
+                $datos=array
+                (
+                    'name'=>'username',
+                    'id'=>'username',
+                    'value'=>'leonardo',
+                    'maxlength'=>'100',
+                    'class'=>'form-control'
+                );
+                echo form_input($datos,'',$js);
+                ?>
+        </div>
+    </div>
+        <div class="row">
+            <div class="col-md-5">
+                <label class="control-label">Campo input password text: </label><?php
+                //$js='onClick-"algunaFuncion()"';
+                $datos=array
+                (
+                    'name'=>'pass',
+                    'type'=>'password',
+                    'id'=>'pass',
+                    'maxlength'=>'100',
+                    'class'=>'form-control'
+                );
+                echo form_input($datos);
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <label class="control-label">Campo File </label><?php
+                //$js='onClick-"algunaFuncion()"';
+                $datos=array
+                (
+                    'name'=>'archivo',
+                    'id'=>'file',
+                    'type'=>'file',
+                    //'class'=>'form-control'
+                );
+                echo form_input($datos);
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <label class="control-label">Text Area </label><?php
+                //$js='onClick-"algunaFuncion()"';
+                $datos=array
+                (
+                    'name'=>'textarea',
+                    'id'=>'textarea',
+                    'type'=>'textarea',
+                    'class'=>'form-control'
+                );
+                echo form_textarea($datos);
+                ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                <label class="form-group">Campo de tipo Select:</label>
+                <select class="form-control" name="paises">
+                    <option value="chile">Chile</option>
+                    <option value="venezuela">Venezuela</option>
+                    <option value="ecuador">Ecuador</option>
+                    <option value="peru">Perú</option>
+                </select>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-5">
+                <?php
+                $datos=array
+                (
+                    'name'=>'boton',
+                    'id'=>'boton',
+                    'type'=>'submit',
+                    'class'=>"btn btn-primary btn-md",
+                    'value'=>'Enviar Mensaje'
+                );
+                echo form_submit($datos);
+                ?>
+            </div>
+        </div>
     <?php
     echo form_close();
     ?>
